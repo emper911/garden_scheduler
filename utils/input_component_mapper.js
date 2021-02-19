25daa594a7f27745c82c1cef29c3cd1168da0f00
emper911 <r.suzuki@queensu.ca>
2021-02-19 00:51:12 -0500

    organised input mapping and output mapping

---
+++ utils/input_component_mapper.js
@@ -0,0 +1,10 @@
+import { DHT } from '../components/input_components';
+
+export const input_component_mapper = (component) => {
+  switch (component.type) {
+    case 'dht':
+      return { type: 'dht', component: new DHT(component.config) }
+    default:
+      return { type: null, component: null };
+  }
+};
---
+++ utils/output_component_mapper.js
@@ -0,0 +1,11 @@
+import { Light } from '../components/output_components';
+
+export const output_component_mapper = (component) => {
+    switch (component.type) {
+      case 'light':
+        return { type: 'light', component: new Light(component.config) }
+      default:
+        return { type: null, component: null };
+    }
+  };
+  
