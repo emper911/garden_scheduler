a82e54ffdc7cfabcc757043c9489b5d9ec738011
emper911 <r.suzuki@queensu.ca>
2021-02-21 18:12:27 -0500

    Wrote schedule controller. Will need to test as next step

---
+++ controllers/scheduleController.js
@@ -0,0 +1,102 @@
+import { CronJob, CronTime } from 'cron';
+import config from '../config/application_config';
+import { output_component_mapper } from '../utils/output_component_mapper';
+
+const { gpio_config } = config;
+const OUTPUT_COMPONENT_MAPPING = gpio_config.output.map(output_component_mapper);
+let ACTIVE_JOBS = [];
+
+const getJob = (id) => id ? ACTIVE_JOBS.find(j => j.id === id) : null;
+const setJob = (job) => ACTIVE_JOBS.push(job);
+const removeJob = (id) => ACTIVE_JOBS = ACTIVE_JOBS.filter(j => j.id !== id);
+const getJobInfo = (id) => {
+  const existingJob = getJob(id);
+  if (existingJob) return { id, sched: existingJob.sched };
+  else return null;
+};
+
+// will have to account for non power type actions
+const createJob = (sched, component) => {
+  const returnJob = {};
+  returnJob.id = `${sched.type}-${new Date().getUTCDate()}`;
+  returnJob.job = new CronJob(sched.time, component.power_control.bind(null, true), component.power_control.bind(null, false), sched.timezone);
+  returnJob.sched = sched;
+  setJob(returnJob);
+};
+
+const rescheduleJob = (sched, job) => {
+  job.setTime(new CronTime(sched.time));
+};
+
+const cancelJob = (id, job) => {
+  job.stop();
+  removeJob(id);
+}
+
+const cronJobManager = (sched, component) => {
+  const schedJobId = sched.jobId;
+  const existingJob = schedJobId !== undefined ? getJob(schedJobId) : null;
+  switch (sched.schedAction) {
+    case 'CREATE':
+      createJob(sched, component);
+    case 'RESCHEDULE':
+      if (existingJob) rescheduleJob(sched, existingJob);
+      break;
+    case 'CANCEL':
+      if (existingJob) cancelJob(schedJobId, existingJob);
+      break;
+    default:
+      break;
+  }
+}
+
+export const getOne = (req, res) => {
+  const { jobId = '' } = req.params;
+  if (!jobId) res.status(400).send({ message: 'Cannot find job' });
+  const existingJobInfo = getJobInfo(jobId);
+  if (existingJobInfo) res.send(existingJobInfo);
+  else res.status.send({ message: 'Cannot find job info'});
+};
+
+export const getMany = (req, res) => {
+  const { jobIdArray = [] } = req.body;
+  try {
+    const activeJobInfoPromise = jobIdArray.map(async id => getJobInfo(id));
+    const activeJobInfo = await Promise.all(activeJobInfoPromise);
+    res.send(activeJobInfo);
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const activeJobInfoPromise = ACTIVE_JOBS.map(async j => ({ id: j.id, sched: j.sched}));
+    const activeJobInfo = await Promise.all(activeJobInfoPromise);
+    res.send(activeJobInfo);
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+}
+
+export const scheduleOne = (req, res) => {
+  const { schedule } = req.body;
+  try {
+    const component = OUTPUT_COMPONENT_MAPPING.find(comp => comp.id === schedule.id)?.component;
+    cronJobManager(schedule, component);
+  } catch (err) {
+    res.status(500).send({'message': err});
+  }
+}
+
+export const scheduleMany = (req, res) => {
+  const { scheduleArray = [] } = req.body;
+  try {
+    scheduleArray.map((sched) => {
+      const component = OUTPUT_COMPONENT_MAPPING.find(comp => comp.id === sched.id)?.component;
+      cronJobManager(sched, component);
+    });
+  } catch (err) {
+    res.status(500).send({'message': err});
+  }
+};
--- index.js
+++ index.js
@@ -1,49 +1,42 @@
 import express from 'express';
-import schedule from 'node-schedule';
 import config from './config/application_config.js';
 import { logger } from './utils/express_helpers.js';
 import { input_component_mapper } from './utils/input_component_mapper';
-import { output_component_mapper } from './utils/output_component_mapper';
+import * as ScheduleController from './controllers/scheduleController';
 
 const { server, gpio_config } = config;
-const INPUT_COMPONENT_MAPPING = gpio_config.output.map(input_component_mapper);
-const OUTPUT_COMPONENT_MAPPING = gpio_config.input.map(output_component_mapper);
+const INPUT_COMPONENT_MAPPING = gpio_config.input.map(input_component_mapper);
 
 const app = express()
 app.use(logger);
 app.use(express.static('public'));
 const port = server.port;
 
-const ACTIVE_JOBS = [];
-const SCHEDULE = [];
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
-
 
 app.listen(port, () => {
   console.log(`Hydro Controller  listening at http://localhost:${port}`)
 });
 
 
-app.get('/state', async (req, res) => {
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Connection", "keep-alive");
-  res.flushHeaders(); // flush the headers to establish SSE with client
+// app.get('/state', async (req, res) => {
+//   res.setHeader('Cache-Control', 'no-cache');
+//   res.setHeader('Content-Type', 'text/event-stream');
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Connection", "keep-alive");
+//   res.flushHeaders(); // flush the headers to establish SSE with client
   
-  update_dht_sensor();
-  res.write(`data: ${JSON.stringify(STATE)}`);
+//   update_dht_sensor();
+//   res.write(`data: ${JSON.stringify(STATE)}`);
 
-  // If client closes connection, stop sending events
-  res.on('close', () => {
-      console.log('client dropped me');``
-      res.end();
-  });
-});
-
+//   // If client closes connection, stop sending events
+//   res.on('close', () => {
+//       console.log('client dropped me');``
+//       res.end();
+//   });
+// });
 
 app.get('/sensors', async (req, res) => {
   const { type = '' } = req.body;
@@ -54,31 +47,9 @@ app.get('/sensors', async (req, res) => {
 });
 
 // {hour: 14, minute: 30, dayOfWeek: 0}
-// [ { id, time, power, }]
-app.post('/schedule', async (req, res) => {
-  const { scheduleArray = [] } = req.body;
-  try {
-    scheduleArray.map((sched) => {
-      let returnJob = {}, returnScheduled = {};
-      if (sched.id !== undefined) {
-        const existingJob = ACTIVE_JOBS.filter(j => j.id === sched.id)[0];
-        if (existingJob === undefined) throw Error('job id not found');
-        if (sched.action === 'reschedule') existingJob.reschedule(sched.time);
-        else if (sched.action === 'cancel') existingJob.cancel();
-      } else {
-        const job = schedule.scheduleJob(sched.time, () => {
-          const pin = OUTPUT_PIN_MAPPING[sched.type];
-          if (pin === undefined) throw Error('Output type not found');
-          if (sched.power !== undefined) power_control(pin, sched.power);
-        });
-        const id = `${sched.type}-${new Date().getUTCDate()}`;
-        returnJob = { id, job};
-        ACTIVE_JOBS.push(returnJob);
-        returnScheduled = { id, ...sched };
-        STATE.scheduled.push(returnScheduled);
-      }
-    });
-  } catch (err) {
-    res.status(500).send({'message': err});
-  }
-});
+// [ { id, time, action, timezone }]
+app.get('/schedule/one', ScheduleController.getOne);
+app.get('/schedule/many', ScheduleController.getMany);
+app.get('/schedule/all', ScheduleController.getAll);
+app.post('/schedule/one', ScheduleController.scheduleOne);
+app.post('/schedule/many', ScheduleController.scheduleMany);
